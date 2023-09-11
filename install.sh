ee42e03aac7d2722a67c9ee94b7295ac9e79404e
lorranavf <lorrana.vf@gmail.com>
2023-09-11 19:49:35 -0300

    install

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # from repo apt (ubuntu store)
-sudo apt install mafft
-sudo apt install iqtree
-sudo apt install hmmer
-sudo apt install ncbi-blast+
-sudo apt install emboss
+sudo apt install mafft -y
+sudo apt install iqtree -y
+sudo apt install hmmer -y
+sudo apt install ncbi-blast+ -y
+sudo apt install emboss -y
 
 # from repo pip (python store)
+pyenv install 3.9.12
+pyenv global 3.9.12
 pip install pandas
 pip install biopython
 pip install pybiolib
@@ -23,11 +25,11 @@ sudo mv cath-resolve-hits.ubuntu-20.04 /usr/bin/cath-resolve-hits
 chmod 755 /usr/bin/cath-resolve-hits
 
 gdown --id 1msW1RpyZHW9pUOANH4phI6tYenh4gQV5 --output deeploc2.tar.gz
-pip install deeploc2.tar.gz
+tar -xzvf deeploc2.tar.gz
+pip install deeploc2_package/
 
 gdown --id 1gbTfAi_i6HnBGkLtvbo6_nocKUltOeo9 --output signalp6.tar.gz
 tar -xzvf signalp6.tar.gz
 pip install signalp6_fast/signalp-6-package/
 SIGNALP_DIR=$(python3 -c "import signalp; import os; print(os.path.dirname(signalp.__file__))" )
 sudo cp -r signalp6_fast/signalp-6-package/models/* $SIGNALP_DIR/model_weights/
-
